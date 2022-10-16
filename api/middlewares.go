b41b5f9f04d7e9dbe646ed650fe533abcf59e134
NeverlandMJ <khasanovasumbula@gmail.com>
2022-10-16 10:37:36 +0500

    adha

--- api/middlewares.go
+++ api/middlewares.go
@@ -1,18 +1,59 @@
 package api
 
 import (
+	"context"
+	"crypto/md5"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Authentication(c *gin.Context) {
+func (h Handler) Authentication(c *gin.Context) {
 	key := c.GetHeader("Key")
 	sign := c.GetHeader("Sign")
-	if key == "" || sign == "" {
+	if key == "" ||  sign== "" {
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"isOk":    false,
-			"message": "user is unauthenticated",
+			"message": "1user is unauthenticated",
+		})
+		c.Abort()
+		return
+	}
+
+	got, err := h.srvc.GetUser(context.Background(), key)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"isOk":    false,
+			"message": "1user is unauthenticated",
+		})
+		c.Abort()
+		return
+	}
+
+	r := c.Request.URL
+	url := r.String()
+
+	body := []byte{}
+	c.Request.Body.Read(body)
+
+	secretByte := md5.Sum([]byte(c.Request.Method + url + string(body) + got.Secret))
+
+	secret := fmt.Sprintf("%x", secretByte)
+
+	if secret != sign {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"isOk":    false,
+			"message": "1user is unauthenticated",
+		})
+		c.Abort()
+		return
+	}
+
+	if secret != sign {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"isOk":    false,
+			"message": "1user is unauthenticated",
 		})
 		c.Abort()
 		return
